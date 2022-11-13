a0450ec6a5f22e37c91d759b2d344daac8d716fc
matheus aragao] <matheusaragaofs@gmail.com>
2022-11-13 17:14:27 -0300

    ignore warnings from recoil until fix it

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,41 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil'
-function MyApp({ Component, pageProps: { session , ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </RecoilRoot>
     </SessionProvider>
   )
 }
+
+
+const memoize = (fn: { (console: any): any; (arg0: any): any }) => {
+  let cache = {} as any;
+  return (...args: any[]) => {
+    let n = args[0];
+    if (n in cache) {
+      return cache[n];
+    }
+    else {
+      let result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  }
+}
+
+
+// ignore in-browser next/js recoil warnings until its fixed.
+const mutedConsole = memoize((console: any) => ({
+  ...console,
+  warn: (...args: (string | string[])[]) => args[0].includes('Duplicate atom key')
+    ? null
+    : console.warn(...args)
+}))
+
+global.console = mutedConsole(global.console);
 
 export default MyApp
